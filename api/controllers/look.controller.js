e99b4937cece614766efa834eb69edd24d3ca62f
Fernand <fernando.vgarcia@hotmail.com>
2020-06-07 18:48:08 -0300

    Verification added. Repo deleted.

--- api/controllers/look.controller.js
+++ api/controllers/look.controller.js
@@ -1,18 +1,30 @@
-const lookRepo = require('../repos/look.repo');
+const Look = require('../models/look.model');
 
 // Dica: você pode usar req.user para acessar informações do usuário que está fazendo a request.
 
 exports.getAll = async (req, res) => {
     try {
+        const {skip = 0, limit = 20} = req.query;
 
-        const { _id } = req.user;
-        
-        const looks = await lookRepo.getAll(_id);
+        if (limit > 50) {
+            return res.status(400).send({message: "BAD_REQUEST: Limit shoud not be bigger than 50"})
+        }
 
-        return res.status(200).send(looks);
-        
-    }
-    catch(err) {
+        const looks = await Look
+            .find({owner: req.user})
+            .sort({createdAt: -1})
+            .populate(['clothe_torso', 'clothe_leg', 'clothe_feet'])
+            .skip(parseInt(skip))
+            .limit(parseInt(limit));
+
+        const amountOfLooks = await Look.countDocuments({owner: req.user});
+
+        res.status(200).send({
+            totalAmount: amountOfLooks,
+            retrievedAmount: looks.length,
+            data: looks
+        });
+    } catch (err) {
         console.error(err, err.message, err.stack);
 
         return res.status(500).send({
@@ -23,20 +35,31 @@ exports.getAll = async (req, res) => {
 
 exports.getById = async (req, res) => {
     try {
+        const {lookId} = req.params;
 
-        const { _id } = req.user;
-        
-        const { lookId } = req.params;
+        const look = await Look.findById(lookId)
+            .populate(['clothe_torso', 'clothe_leg', 'clothe_feet']);
 
-        const look = await lookRepo.getById(lookId, _id);
+        // Verifica se o Look existe
+        if (!look) {
+            return res.status(404).send({
+                message: "Look not found with id " + lookId
+            });
+        }
 
-        return res.status(200).send(look);
+        // Verifica se o Look pertence ao usuário
+        if (look.owner != req.user._id) {
+            return res.status(403).send({
+                message: "You can't access this Look " + lookId
+            });
+        }
 
-    }
-    catch(err) {
+        res.status(200).send(look);
+
+    } catch (err) {
         console.error(err, err.message, err.stack);
 
-        if(err.kind === 'ObjectId') {
+        if (err.kind === 'ObjectId') {
             return res.status(404).send({
                 message: "Look not found with id " + req.params.lookId
             });
@@ -50,17 +73,22 @@ exports.getById = async (req, res) => {
 
 exports.create = async (req, res) => {
     try {
-    
-        const look = await lookRepo.create(req.body);
+        const look = new Look(req.body);
+        look.owner = req.user;
 
-        return res.status(201).send(look);
-
-    }
-    catch(err) {
+        await look.save();
+        res.status(201).send(look);
+    } catch (err) {
         console.error(err, err.message, err.stack);
 
-        return res.status(500).send({
-            message: err || "An error occured when creating the look."
-        });
+        if (err.name == 'ValidationError') {
+            res.status(400).send({
+                message: err.message
+            });
+        } else {
+            res.status(500).send({
+                message: err || "An error occured when creating the look."
+            });
+        }
     }
 };
